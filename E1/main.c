29beb31e7b762321e280bbfbb3398ff800697561
Edilton-Damasceno <ediltondamasceno@ufba.br>
2026-03-21 23:13:46 -0300

    Upando E1

--- E1/main.c
+++ E1/main.c
@@ -1,6 +1,30 @@
+#include <stdio.h>
 #include "token.h"
 
-int main(int argc, char **argv) {
+extern int yylex();
+extern char *lexema;
 
+int main() {
+    int token;
+
+    while ((token = yylex()) != EOL) {
+        switch (token) {
+            case NUM:
+                printf("categoria do token: %d, constante numérica: %s\n", token, lexema);
+                break;
+
+            case PLUS:
+            case MINUS:
+            case TIMES:
+            case DIV:
+                printf("categoria do token: %d\n", token);
+                break;
+
+            case ERROR:
+                printf("erro léxico: caractere inválido\n");
+                break;
+        }
+    }
+
+    return 0;
 }
-
